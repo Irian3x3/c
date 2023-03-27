9aa494ad106f34010b071a26ad7a6d0feb765506
Irian3x3 <irian3x3@gmail.com>
2023-03-26 21:28:50 -0400

    c

---
+++ c.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define _ int
+#define __ main
+#define ___ (
+#define ____ )
+#define _____ {
+#define ______ printf
+#define _______ (
+#define ________ "Hello, World!"
+#define _________ )
+#define __________ ;
+#define ___________ return 
+#define ____________ 0
+#define ______________ ;
+#define _______________ }
+
+_ __ ___ ____ _____ ______ _______ ________ _________ __________ ___________ ____________ ______________ _______________
